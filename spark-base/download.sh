6c36efe88748cab655e7477cc1c9c689377b186c
jayden <jayden@acryl.ai>
2023-09-21 16:44:41 +0900

    3.3.0 ver

--- spark-base/download.sh
+++
@@ -1,4 +0,0 @@
-# /bin/bash
-
-wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.170/aws-java-sdk-bundle-1.12.170.jar
-wget https://archive.apache.org/dist/spark/spark-3.1.2/pyspark-3.1.2.tar.gz
